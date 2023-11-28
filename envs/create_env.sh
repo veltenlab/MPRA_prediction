d65a43eb483e43eae4888da7f629d720ece912e4
felixpachecopastor <pachecopastor.felix@gmail.com>
2023-11-28 16:30:39 +0100

    add create_env contribution scores on bash script

--- envs/create_env.sh
+++ envs/create_env.sh
@@ -7,3 +7,11 @@ conda install -c anaconda pandas
 conda install -c anaconda numpy
 conda install -c anaconda scikit-learn 
 conda install -c anaconda matplotlib
+
+
+mamba create --name contribution_scores python=3.7
+mamba activate contribution_scores
+mamba install -c anaconda tensorflow-gpu=1.14.0
+mamba install -c anaconda keras-gpu=2.2.4
+mamba install shap=0.29.3
+mamba install h5py=2.10.0
