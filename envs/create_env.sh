50b1c558064e134ed128241614a1b2056f19ec8b
felixpachecopastor <pachecopastor.felix@gmail.com>
2023-12-04 11:45:25 +0100

    fix env contribution_scores issue

--- envs/create_env.sh
+++ envs/create_env.sh
@@ -9,9 +9,12 @@ conda install -c anaconda scikit-learn
 conda install -c anaconda matplotlib
 
 
-mamba create --name contribution_scores python=3.7
-mamba activate contribution_scores
-mamba install -c anaconda tensorflow-gpu=1.14.0
-mamba install -c anaconda keras-gpu=2.2.4
-mamba install shap=0.29.3
-mamba install h5py=2.10.0
+conda create --name contribution_scores python=3.7
+conda activate contribution_scores
+conda install -c anaconda tensorflow-gpu=1.14.0
+conda install -c anaconda keras-gpu=2.2.4
+conda install shap=0.29.3
+conda install h5py=2.10.0
+pip install ipykernel
+conda install pandas
+conda install matplotlib
